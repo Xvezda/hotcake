141427b503f9d9153a2d4ee51fdac62e56cbd397
Xvezda <xvezda@naver.com>
2020-08-15 04:57:06 +0900

    Add prototype

---
+++ app.js
@@ -0,0 +1,9 @@
+class App {
+  constructor() {
+    const heading = document.createElement('h1')
+    heading.textContent = 'It works!'
+    document.body.appendChild(heading)
+  }
+}
+
+new App()
---
+++ index.js
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2020 Xvezda <xvezda@naver.com>
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+(function() {
+  var initialized = false;
+
+  function normalize(url) {
+    var virtualLink = document.createElement('a');
+    virtualLink.href = url;
+    return virtualLink.href;
+  }
+
+  function poll(url, callback) {
+    var xhr = new XMLHttpRequest();
+    var newUrl = url + ((url.indexOf('?') !== -1 ? '&' : '?') +
+      (new Date).getTime());
+    xhr.open('GET', newUrl);
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+
+    xhr.onreadystatechange = function(event) {
+      switch (xhr.readyState) {
+        case XMLHttpRequest.DONE:
+          callback(event.target.response, url);
+          break;
+        default:
+          // throw new Error('unexpected state code')
+          break;
+      }
+    };
+
+    xhr.send(null);
+  }
+
+  function Eazel(options) {
+    initialized = true;
+
+    var cache = {};
+    var defaultOptions = {
+      targets: ['./'],
+      interval: 3000,
+    };
+    options = options || {}
+
+    for (var key in defaultOptions) {
+      if (!(key in options)) {
+        options[key] = defaultOptions[key];
+      }
+    }
+
+    function polling(url) {
+      setTimeout(function() {
+        poll(url, function(response) {
+          var key = normalize(url);
+          var oldData = cache[key];
+          var newData = response;
+          if (oldData !== undefined && oldData !== newData) {
+            return window.location.reload();
+          }
+          cache[key] = newData;
+          polling(url);
+        });
+      }, options.interval);
+    }
+
+    // Initialize caches
+    for (var i = 0; i < options.targets.length; ++i) {
+      poll(options.targets[i], function(response, url) {
+        cache[normalize(url)] = response;
+        polling(url);
+      });
+    }
+  }
+
+  // Expose
+  if (typeof window !== 'undefined' && !('Eazel' in window)) {
+    window.Eazel = Eazel;
+  }
+
+  window.onload = function(event) {
+    if (!initialized) {
+      window.Eazel.o = new Eazel();  // Active instance
+    }
+  };
+})();
